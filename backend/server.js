25c61894b2be862d503018fd71d63a05bad4ef97
chandupaS <isthisperfecto24@gmail.com>
2026-04-27 23:09:56 +0530

    port

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
- .then(() => console.log('MongoDB connected'))
- .catch(err => console.log(err));
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
 const itemRoutes = require('./routes/items');
 app.use('/api/items', itemRoutes);
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port
 ${PORT}`));
